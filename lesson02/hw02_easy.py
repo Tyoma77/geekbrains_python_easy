75b1f173f3404e40ae0458a78d14b52e6d16a93b
tyoma77 <tyoma77@ya.ru>
2018-07-26 01:23:37 +0300

    решены задния normal

--- lesson02/hw02_easy.py
+++ lesson02/hw02_easy.py
@@ -1,11 +1,13 @@
 import random
 
+
 def create_list(x):
-	tmp_list = []
-	while  x > 0:
-		tmp_list.append(random.randint(1, 20))
-		x -= 1
-	return tmp_list
+    tmp_list = []
+    while x > 0:
+        tmp_list.append(random.randint(1, 20))
+        x -= 1
+    return tmp_list
+
 
 # Задача-1:
 # Дан список фруктов. Напишите программу, выводящую фрукты в виде нумерованного списка выровненного по правой сторне
@@ -22,25 +24,21 @@ def create_list(x):
 fruits = ["яблоко", "банан", "киви", "арбуз"]
 i = 0
 while i < len(fruits):
-    print('{}. {:>7}'.format(i+1, fruits[i]))
+    print('{}. {:>7}'.format(i + 1, fruits[i]))
     i += 1
 
 # Задача-2:
 # Даны два произвольные списка. Удалите из первого списка элементы присутствующие во втором списке
 
-list_1 = []
-list_2 = []
-
 list_1 = create_list(random.randint(2, 10))
 list_2 = create_list(random.randint(2, 10))
 
 for k in list_2:
-	while list_1.count(k) > 0:
-		list_1.remove(k)
-		
+    while list_1.count(k) > 0:
+        list_1.remove(k)
+
 print(list_1)
 print(list_2)
-
 
 # Задача-3:
 # Дан произвольный список из целых чисел. Получите НОВЫЙ список из элементов исходного выполнив следующие условия:
