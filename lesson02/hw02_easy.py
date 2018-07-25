8911d8cf4c5ccb474895bac59ff521f075a08295
Tyoma77 <tyoma77@ya.ru>
2018-07-25 15:35:22 +0300

    Update hw02_easy.py
    решены все задания easy

--- lesson02/hw02_easy.py
+++ lesson02/hw02_easy.py
@@ -1,4 +1,12 @@
 import random
+
+def create_list(x):
+	tmp_list = []
+	while  x > 0:
+		tmp_list.append(random.randint(1, 20))
+		x -= 1
+	return tmp_list
+
 # Задача-1:
 # Дан список фруктов. Напишите программу, выводящую фрукты в виде нумерованного списка выровненного по правой сторне
 # Пример:
@@ -20,16 +28,27 @@ while i < len(fruits):
 # Задача-2:
 # Даны два произвольные списка. Удалите из первого списка элементы присутствующие во втором списке
 
+list_1 = []
+list_2 = []
+
+list_1 = create_list(random.randint(2, 10))
+list_2 = create_list(random.randint(2, 10))
+
+for k in list_2:
+	while list_1.count(k) > 0:
+		list_1.remove(k)
+		
+print(list_1)
+print(list_2)
+
+
 # Задача-3:
 # Дан произвольный список из целых чисел. Получите НОВЫЙ список из элементов исходного выполнив следующие условия:
 # если элемент кратный двум, то разделить его на 4, если не кратен, то умножить на два.
 
-j = random.randint(2, 20)
-num_list = []
-while j > 0:
-    num_list.append(random.randint(1, 20))
-    j -= 1
+num_list = create_list(random.randint(2, 10))
 new_num_list = []
+
 for num in num_list:
     if num % 2 == 0:
         new_num_list.append(num / 4)
