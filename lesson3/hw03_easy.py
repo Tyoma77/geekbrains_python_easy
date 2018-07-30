87f3a1111f5988c7a3d29e7f0a3b2e964f398b22
tyoma77 <tyoma77@ya.ru>
2018-07-30 23:23:36 +0300

    Решено easy

--- lesson3/hw03_easy.py
+++ lesson3/hw03_easy.py
@@ -4,8 +4,20 @@
 # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
+
 def my_round(number, ndigits):
-    pass
+    tmp_list = str(number).split('.')
+    f_digit = int(tmp_list[0])
+    s_digit = 0
+
+    for i in range(0, ndigits):
+        s_digit += int((tmp_list[1])[i]) / (10 ** (i + 1))
+        if i == ndigits - 1:
+            if int((tmp_list[1])[i+1]) >= 5:
+                s_digit += 1 / (10 ** (i + 1))
+
+    digit = s_digit + f_digit
+    return digit
 
 
 print(my_round(2.1234567, 5))
@@ -20,20 +32,21 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
+    if len(str(ticket_number)) != 6:
+        return "Номер не шестизначный"
     t1 = 0
     t2 = 0
-    i = 0
+
     ticket_list = str(ticket_number)
 
-    while i < 2:
+    for i in range(0, 2):
         t1 += int(ticket_list[i])
         t2 += int(ticket_list[i + 2])
-        i += 1
 
     if t1 == t2:
-        return True
+        return "Счастливый"
     else:
-        return False
+        return "Несчастливый"
 
 
 print(lucky_ticket(123006))
