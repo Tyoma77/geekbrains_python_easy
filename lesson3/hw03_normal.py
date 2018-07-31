c7b33025afc5d94067c87cf78bee8a1cb8895306
tyoma77 <tyoma77@ya.ru>
2018-07-31 07:57:04 +0300

    Решена первая задача normal

--- lesson3/hw03_normal.py
+++ lesson3/hw03_normal.py
@@ -2,9 +2,15 @@
 # Напишите функцию, возвращающую ряд Фибоначчи с n-элемента до m-элемента.
 # Первыми элементами ряда считать цифры 1 1
 
-def fibonacci(n, m):
-    pass
 
+def fibonacci(n, m):
+    fib_list = [1, 1]
+    for i in range(1, m):
+        fib_list.append(fib_list[i] + fib_list[i - 1])
+
+    return fib_list[n-1: m]
+
+print(fibonacci(3, 10))
 
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
