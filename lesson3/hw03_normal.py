0134d061f9f26ccbc5c82aef569d0c3dfc3d5208
tyoma77 <tyoma77@ya.ru>
2018-08-01 08:19:15 +0300

    Решена четвёртая задача normal

--- lesson3/hw03_normal.py
+++ lesson3/hw03_normal.py
@@ -1,3 +1,6 @@
+import math
+
+
 # Задание-1:
 # Напишите функцию, возвращающую ряд Фибоначчи с n-элемента до m-элемента.
 # Первыми элементами ряда считать цифры 1 1
@@ -8,9 +11,11 @@ def fibonacci(n, m):
     for i in range(1, m):
         fib_list.append(fib_list[i] + fib_list[i - 1])
 
-    return fib_list[n-1: m]
+    return fib_list[n - 1: m]
+
 
 print(fibonacci(3, 10))
+
 
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
@@ -19,16 +24,18 @@ print(fibonacci(3, 10))
 
 
 def sort_to_max(origin_list):
-    n = 1 
-	while n < len(origin_list):
-	    for i in range(len(origin_list) - n):
-	        if origin_list[i] > origin_list[i + 1]:
-	            origin_list[i], origin_list[i + 1] = origin_list[i + 1], origin_list[i]
+    n = 1
+    while n < len(origin_list):
+        for i in range(len(origin_list) - n):
+            if origin_list[i] > origin_list[i + 1]:
+                origin_list[i], origin_list[i + 1] = origin_list[i + 1], origin_list[i]
         n += 1
-	    
+
     print(origin_list)
-	    
+
+
 sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
+
 
 # Задача-3:
 # Напишите собственную реализацию стандартной функции filter.
@@ -38,3 +45,27 @@ sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
 # Определить, будут ли они вершинами параллелограмма.
+
+def get_length(a, b):
+    length = math.sqrt((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2)
+    return length
+
+
+def check_para(a1, a2, a3, a4):
+    a1_a2 = get_length(a1, a2)
+    a3_a4 = get_length(a3, a4)
+    a1_a3 = get_length(a1, a3)
+    a2_a4 = get_length(a2, a4)
+    if a1_a2 == a3_a4 and a1_a3 == a2_a4:
+        return 'Является паралелограммом'
+    else:
+        return 'не является паралелограммом'
+
+
+A1 = (1, 3)
+A3 = (-1, 4)
+A2 = (4, 7)
+A4 = (2, 8)
+
+
+print(check_para(A1, A2, A3, A4))
