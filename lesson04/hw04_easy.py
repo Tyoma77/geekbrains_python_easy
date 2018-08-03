8d59b0fe8c72593f30cb6148b31a62ed78375a2d
tyoma77 <tyoma77@ya.ru>
2018-08-03 22:59:13 +0300

    преобразования

--- lesson04/hw04_easy.py
+++ lesson04/hw04_easy.py
@@ -6,18 +6,21 @@
 # квадратами элементов исходного списка
 # [1, 2, 4, 0] --> [1, 4, 16, 0]
 
-def first(list):
-  n_list = [i**2 for i in list]
-  return n_list
+
+def first(li):
+    n_list = [i ** 2 for i in li]
+    return n_list
+
 
 # Задание-2:
 # Даны два списка фруктов.
 # Получить список фруктов, присутствующих в обоих исходных списках.
 
 def second(fst_list, scn_list):
-  unique = [str(i).lower() for i in fst_list] + [str(j).lower() for j in scn_list]
-  unique = list(set(unique))
-  return unique
+    unique = [str(i).lower() for i in fst_list] + [str(j).lower() for j in scn_list]
+    unique = list(set(unique))
+    return unique
+
 
 # Задание-3:
 # Дан список, заполненный произвольными числами.
@@ -26,17 +29,17 @@ def second(fst_list, scn_list):
 # + Элемент положительный
 # + Элемент не кратен 4
 
-def third(num_list):
-  unique_num_list = [i for i in num_list if i%3 == 0 and i >0 and i%4 != 0]
-  return unique_num_list
+def third(numb_list):
+    unique_num_list = [i for i in numb_list if i % 3 == 0 and i > 0 and i % 4 != 0]
+    return unique_num_list
 
 
 if __name__ == "__main__":
-  print(first([1, 2, 4, 0]))
+    print(first([1, 2, 4, 0]))
 
-  list_1 = ['asdasdad', 'asdasda'] 
-  list_2 =  ['asdasdasdasdc', 'aasdasdasdfxv', 'asdasda']
-  print(second(list_1, list_2))
-  
-  num_list = [i for i in range(-5, 45)]
-  print(third(num_list))
+    list_1 = ['asdasdad', 'asdasda']
+    list_2 = ['asdasdasdasdc', 'aasdasdasdfxv', 'asdasda']
+    print(second(list_1, list_2))
+
+    num_list = [i for i in range(-5, 45)]
+    print(third(num_list))
---
+++ lesson04/hw04_normal.py
@@ -0,0 +1,59 @@
+import re
+
+
+# Задание-1:
+# Вывести символы в нижнем регистре, которые находятся вокруг
+# 1 или более символов в верхнем регистре.
+# Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
+# Решить задачу двумя способами: с помощью re и без.
+
+
+# Задание-2:
+# Вывести символы в верхнем регистре, слева от которых находятся
+# два символа в нижнем регистре, а справа - два символа в верхнем регистре.
+# Т.е. из строки 
+# "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
+# нужно получить список строк: ['AY', 'NOGI', 'P']
+# Решить задачу двумя способами: с помощью re и без.
+
+
+def first_with_re(ln):
+    new_line = re.split('[A-Z]', ln)
+    new_line = [i for i in new_line if i != '']
+
+    return new_line
+
+
+def second_with_re(ln):
+    new_line = re.findall(r'[a-z]{2}[A-Z]+[A-Z]{2}', ln)
+    l1 = [re.sub('[a-z]{2}', '', i) for i in new_line]
+    l2 = [re.sub('[A-Z]{2}$', '', i) for i in l1]
+    return l2
+
+
+if __name__ == "__main__":
+    line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUl' \
+           'TLeclMwAoktKlfUBJHPsnawvjPhfgewVzKTUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqnLx' \
+           'ooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIVjXapzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZG' \
+           'kdOaQYLVBfsGSAfJMchgBWAsGnBnWetekUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfCvzQWrM' \
+           'MsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbRuXbJrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdev' \
+           'HhBJMBSShDqbjAuDGTTrSXZywYkmjCCEUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOnLfBtQQS' \
+           'hTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGSeuTSkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWt' \
+           'ZCEXZCnZjLeMiFlxnPkqfJFbCfKCuUJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWHuXBqHFjvi' \
+           'huNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvd' \
+           'DyleXylnKBfLCjLHntltignbQoiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+
+    print(first_with_re(line))
+
+    line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeip' \
+             'EUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVfzKTUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVj' \
+             'fSAHqnLxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIVjXapzGOrfinzzsNMtBIOclwbfRzy' \
+             'tmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnWetekUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCf' \
+             'kVIAQaAbfCvzQWrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbRuXbJrwTRNyAxDctszKjSnn' \
+             'daFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkmjCCEUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxii' \
+             'hwsCdBViEQBHQaOnLfBtQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGSeuTSkyjIGsiWLAL' \
+             'HUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCfKCuUJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZM' \
+             'WSVQyzenWoGxyGPvbnhWHuXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFaXiUWgsKQ' \
+             'rDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+
+    print(second_with_re(line_2))
